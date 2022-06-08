1375a6c42cfe7bfa0f63df677512ee3bbd671399
MarkTempelman <43167879+MarkTempelman@users.noreply.github.com>
2022-06-08 12:02:03 +0200

    added sematext

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavigationStart, Router} from '@angular/router';
+
+declare global {
+  // tslint:disable-next-line:ban-types
+  interface Window { strum: Function; }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'user-interface';
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        window.strum('routeChange', event.url);
+      }
+    });
+  }
 }
